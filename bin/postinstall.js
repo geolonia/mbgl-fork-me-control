b0faabbf8933ec9b86e44a46215467990e70333f
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-07-30 15:05:31 +0900

    Add deprecation for the @tilecloud package

---
+++ bin/postinstall.js
@@ -0,0 +1,25 @@
+const chalk = require("chalk");
+const LABEL = chalk.yellow.bold.bgBlack("[WARN]");
+const meta = require("../package.json");
+
+const main = () => {
+  const [, org, name] = /^@(.+)\/(.+)/.exec(meta.name) || [];
+
+  if (org !== "tilecloud") {
+    return;
+  } else {
+    const oldName = chalk.yellow.bold.bgBlack(`@${org}/${name}`);
+    const newName = chalk.yellow.bold.bgBlack(`@geolonia/${name}`);
+
+    const lines = [
+      `The package ${oldName} is deprecated and no longer updated.`,
+      `Please use ${newName} instead.`
+    ];
+
+    const message = lines.map(line => `${LABEL} ${line}`).join("\n");
+
+    process.stdout.write(`${message}\n`);
+  }
+};
+
+main();
