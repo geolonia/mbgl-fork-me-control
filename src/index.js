80735a58cb1b06c7042873e16ce0a0ae5974e544
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-08-02 13:21:22 +0900

    Fix how to display control container: use  class to simplify

--- src/index.js
+++ src/index.js
@@ -7,15 +7,19 @@ class ForkMeConntrol {
 
   onAdd() {
     this.container = document.createElement('div')
-    this.container.className = 'mapboxgl-ctrl'
+    this.container.className = 'mapboxgl-ctrl mapboxgl-ctrl-group'
 
     const img = 'https://s3.amazonaws.com/github/ribbons/forkme_left_darkblue_121621.png'
-    const style = 'position: absolute; top: -149px; left: 0; border: 0;'
 
-    this.container.innerHTML = `<a><img style="${style}" src="${img}" alt="Fork me on GitHub"></a>`
+    this.container.innerHTML = `<a><img style="" src="${img}" alt="Fork me on GitHub"></a>`
     this.container.querySelector('a').href = this.url
-
-    document.querySelector('.mapboxgl-ctrl-top-left').style.top = '149px'
+    this.container.setAttribute('style', `
+      margin: 0;
+      background: none;
+      border: none;
+      border-radius: 0;
+      box-shadow: none;
+    `)
 
     return this.container;
   }
