06c03a1d0f3c0882691733c843a4002870990116
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-08-02 10:30:01 +0900

    Patch for mapbox-gl-js>=1.0.0, following to style breaking change

--- src/index.js
+++ src/index.js
@@ -10,7 +10,20 @@ class ForkMeConntrol {
     this.container.className = 'mapboxgl-ctrl'
 
     const img = 'https://s3.amazonaws.com/github/ribbons/forkme_left_darkblue_121621.png'
-    const style = 'position: absolute; top: -149px; left: 0; border: 0;'
+
+    const styles = {
+      default: 'position: absolute; top: -159px; left: -10px; border: 0;',
+      '<1.0.0': 'position: absolute; top: -149px; left: 0; border: 0;'
+    }
+
+    let style = styles.default
+
+    if (window.mapboxgl) {
+      const major = parseInt(window.mapboxgl.version.split('.')[0])
+      if (!isNaN(major) && major < 1) {
+        style = styles['<1.0.0']
+      }
+    }
 
     this.container.innerHTML = `<a><img style="${style}" src="${img}" alt="Fork me on GitHub"></a>`
     this.container.querySelector('a').href = this.url
