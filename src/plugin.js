e748a7e199d1383c71c48972b3eb96ed5fc195e5
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-08-04 11:23:37 +0900

    Add geolonia-fork-me-disaled data attriute to control plugin enabled/disabled

--- src/plugin.js
+++ src/plugin.js
@@ -3,6 +3,12 @@ import ForkMeControl from "./index";
 const { registerPlugin } = window.geolonia || window.tilecloud || {};
 typeof registerPlugin === "function" &&
   registerPlugin((map, target) => {
-    const { url = "#!" } = target.dataset || {};
-    map.addControl(new ForkMeControl({ url }));
+    const { url = "#", geoloniaForkMeDisabled } = target.dataset || {};
+    geoloniaForkMeDisabled || map.addControl(new ForkMeControl({ url }));
   });
+
+// NOTE: plugin interfaces
+// - Use `window.geolonia.registerPlugin(Plugin)`
+// Plugin: (map: Mapbox.map, target: HTMLElement) => void
+
+// - (recommended) Plugin should be disabled with `data-${namespace}-${plugin-name}-disabled="on"` attriute.
