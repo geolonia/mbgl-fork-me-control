ca5b21b66ab882aa746ae3c242dde26b78813a9b
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-08-05 11:18:53 +0900

    Revert "Merge branch 'display-image-for-multiple-maps' into plugin-feature"
    This reverts commit 8a3e463da72ab4d8533c078c9645cb040c25e81b, reversing
    changes made to 2ea0ee5b7be8f9e3115645a1520187873d49c399.

--- src/index.js
+++ src/index.js
@@ -7,18 +7,15 @@ class ForkMeConntrol {
 
   onAdd() {
     this.container = document.createElement('div')
-    this.container.className = 'mapboxgl-ctrl mapboxgl-ctrl-group'
+    this.container.className = 'mapboxgl-ctrl'
 
     const img = 'https://s3.amazonaws.com/github/ribbons/forkme_left_darkblue_121621.png'
+    const style = 'position: absolute; top: -149px; left: 0; border: 0;'
 
-    this.container.innerHTML = `<a href="${this.url}"><img src="${img}" alt="Fork me on GitHub"></a>`
-    this.container.setAttribute('style', `
-      margin: 0;
-      background: none;
-      border: none;
-      border-radius: 0;
-      box-shadow: none;
-    `)
+    this.container.innerHTML = `<a><img style="${style}" src="${img}" alt="Fork me on GitHub"></a>`
+    this.container.querySelector('a').href = this.url
+
+    document.querySelector('.mapboxgl-ctrl-top-left').style.top = '149px'
 
     return this.container;
   }
