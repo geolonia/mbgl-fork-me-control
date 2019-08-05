81925781139eff44516a9767e3b502b32cc9737f
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-08-05 11:14:43 +0900

    Some fix

--- src/plugin.js
+++ src/plugin.js
@@ -1,26 +1,26 @@
 import ForkMeControl from "./index";
 const { registerPlugin } = window.geolonia || window.tilecloud || {};
 
-const isEnabled = ({ dataset }, snakeCasedAttributeKey) => {
+const isDisabled = ({ dataset = {} }, snakeCasedAttributeKey) => {
   const attributeKey = snakeCasedAttributeKey
     .split("-")
     .map((phrase, i) =>
       i === 0 ? phrase : phrase[0].toUpperCase() + phrase.slice(1)
     )
     .join("");
+
   const isDefined = attributeKey in dataset;
+  const isDisabled =
+    !isDefined ||
+    (dataset[attributeKey] !== "" &&
+      dataset[attributeKey].toUpperCase() !== "DISABLED");
 
-  const isEnabled =
-    isDefined &&
-    (dataset.attributeKey === "" ||
-      dataset.attributeKey.toUpperCase() === "DISABLED");
-
-  return isEnabled;
+  return isDisabled;
 };
 
 typeof registerPlugin === "function" &&
   registerPlugin((map, target) => {
-    if (isEnabled(map, "geolonia-fork-me-disabled")) {
+    if (!isDisabled(target, "geolonia-fork-me-disabled")) {
       const { url = "#" } = target.dataset || {};
       map.addControl(new ForkMeControl({ url }));
     }
